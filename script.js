19541d17b0b1bbc3a03677c6c19b133f0b8dae88
arafat-ahmed-dev <arafatahmedshlbd194@gmail.com>
2024-09-28 23:46:52 +0600

    done

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
-const inputValue = document.querySelector(".input-value")
+function run() { 
+const submit = document.querySelector(".submit"),
+newGame = document.querySelector(".new"),
+result = document.querySelector("#result");
+let randomNumber = Math.floor(Math.random() * 100),
+previousValue = document.querySelector(".previous-value"),
+data = [];
 
-console.log(inputValue.value);
+submit.addEventListener("click",(e)=> {
+    e.preventDefault();
+    const inputValue = document.querySelector("#guess").value
+    const userValue = parseInt(inputValue);
+    if (isNaN(userValue)) {
+        result.innerHTML = "Please enter a number";
+    }else if(userValue > 0 && userValue < 101){
+        data.push(userValue);
+        previousValue.innerHTML = `Previous values : ${data.join(", ")}`
+    if(userValue > randomNumber){
+        result.innerHTML = "You guessed too high!";
+    }
+    else if (userValue < randomNumber) {
+        result.innerHTML = "You guessed too low!";    
+    }
+    else {
+        result.innerHTML = "You guessed correctly!";
+        submit.disabled = true;
+        newGame.disabled = false;
+        data = [];
+        previousValue.innerHTML = ''
+    }
+    }else{
+        result.innerHTML = "Please enter a number between 1 and 100";
+    }
+    randomNumber = Math.floor(Math.random() * 100);
+})
+newGame.addEventListener("click",()=>{
+    submit.disabled = false;
+    newGame.disabled = true;
+    result.innerHTML = "";
+})
+}
+run();
